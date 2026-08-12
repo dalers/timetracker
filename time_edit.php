27bd57f46303e02257f136417b4157f85192d984
Dale Scott <dale@dalescott.net>
2026-08-12 13:07:56 -0600

    also allow horizontal resizing

--- time_edit.php
+++ time_edit.php
@@ -255,7 +255,7 @@ if ($user->isPluginEnabled('tp')){
 }
 
 // Note control.
-$form->addInput(array('type'=>'textarea','name'=>'note','value'=>$cl_note,'style'=>'resize: vertical;'));
+$form->addInput(array('type'=>'textarea','name'=>'note','value'=>$cl_note,'style'=>'resize: both;'));
 
 // Hidden control for record id.
 $form->addInput(array('type'=>'hidden','name'=>'id','value'=>$cl_id));
